0a8269cc22b647c8a93ae0327068e00ad4bc0483
Ronald Mutua <ronaldmutua617@gmail.com>
2024-08-27 23:19:34 +0300

    created jsx for about and dvbs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,153 @@
 
 export default function About(){
     return (
-        <div>About</div>
+        <div className="container">
+  <h1>History</h1>
+  <p>
+    Talanta ministries was founded in 2015 by Rev Joseph Nzavi, Meggy Nzavi and
+    other families. They two had served with CITAM for close to two decades as
+    pastors in various capacities. They were blessed, released and given cover
+    under as CITAM alliance in Machakos where they started the ministry up to
+    date.
+  </p>
+  <p>
+    With the support of Citam leadership we started Talanta Ministry at Machakos
+    Show Ground in March 2015 . In the course of growth we received an official
+    MOU from Citam to be the first ever alliance of Citam in August 2015. We
+    value this partnership and with the current leadership we have the support
+    to have Talanta ministries registered. We have thus far experienced growth
+    having mature members who are part of the ministry not as just members but
+    real vision owners.
+  </p>
+  <h1>Leadership</h1>
+  <div className="leader">
+    <div className="leader-profile">
+      <img src="#" />
+      <div className="leader-title">
+        <h4>Pastor Joseph Nzavi</h4>
+        <h5>Chairman of the board and Lead pastor</h5>
+      </div>
+    </div>
+    <p>
+      He is a former CITAM pastor and having ministered for nearly a decade in
+      various pastoral positions, the last one being as the founding senior
+      Pastor of Citam Nakuru - he has always felt the need and call to set up a
+      ministry like Citam to reach further to the lost.
+    </p>
+    <p>
+      He is happily married and has three children. He is also an active member
+      in the men&apos;s ministry and is remarkably enthusiastic about outreach and
+      missions. The love also that he has for kids goes without saying as
+      indescribable.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+        <img src="#" />
+      <div className="leader-title">
+        <h4>Doctor Levis Nguku</h4>
+        <h5>Secretary to the board.</h5>
+      </div>
+    </div>
+    <p>
+      Serves in the Men&apos;s ministry, is rapturously paired, has two kids and
+      works in Machakos.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+    <img src="#" />
+      <div className="leader-title">
+        <h4>Andrew Sibairo</h4>
+        <h5>Board&apos;s treasurer</h5>
+      </div>
+    </div>
+    <p>
+      Serves in the Men&apos;s ministry, works with The Navigators Kenya, married and
+      has got two kids.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+    <img src="#" />
+      <div className="leader-title">
+        <h4>Pastor Ruth Kitonyi</h4>
+        <h5>Board member and pastor at TAMI Intl</h5>
+      </div>
+    </div>
+    <p>
+      Serves as the church admin, is an active member of the women&apos;s ministry
+      and is affiliated with the ushering and hospitality ministry as well. She
+      is the mother of one child.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+    <img src="#" />
+      <div className="leader-title">
+        <h4>Josephine Muasa</h4>
+        <h5>Board member</h5>
+      </div>
+    </div>
+    <p>
+      Serves also in ushering and hospitality and is an active women&apos;s ministry
+      member. Has two kids and is gleefully married.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+    <img src="#" />
+      <div className="leader-title">
+        <h4>Nicholas Kioko</h4>
+        <h5>Board member</h5>
+      </div>
+    </div>
+    <p>
+      Serves also in the ushering ministry and is an active member of the men&apos;s
+      ministry. He&apos;s got two kids and is cheerfully matched to his wife.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+    <img src="#" />
+      <div className="leader-title">
+        <h4>Joseph Ngui</h4>
+        <h5>Board member</h5>
+      </div>
+    </div>
+    <p>
+      Chairman of the men&apos;s ministry and is responsible for the infrastrucrtural
+      development and projects in the church.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+    <img src="#" />
+      <div className="leader-title">
+        <h4>Pastor Meggy Hawata</h4>
+        <h5>Board member</h5>
+      </div>
+    </div>
+    <p>
+      Serves actively in Children&apos;s ministry and is an active member of the
+      Women&apos;s ministry as well.She is contentedly coupled and has two kids.
+    </p>
+  </div>
+  <div className="leader">
+    <div className="leader-profile">
+    <img src="#" />
+      <div className="leader-title">
+        <h4>Felistah Rukunya</h4>
+        <h5>Board member</h5>
+      </div>
+    </div>
+    <p>
+      Serves also as a teacher in the Children&apos;s ministry and is an active
+      participant in the women&apos;s ministry. She&apos;s a freelancer and a mother to
+      one.
+    </p>
+  </div>
+</div>
+
     )
 }
--- src/pages/DVBS.jsx
+++ src/pages/DVBS.jsx
@@ -2,6 +2,315 @@
 
 export default function DVBS(){
     return (
-        <div>DVBS</div>
-    )
+        <div className="container">
+  <div className="about-dvbs">
+    <h1 id="dvbs-name">DVBS</h1>
+    <p>
+      Daily Vacational Bible School is, simply put, a <b>BANGER!!</b> A
+      programme that your child wouldn&apos;t wanna miss for the world. From the
+      intensive Bible explication to the dance, hype and games. Not to mention
+      the world -famous art and craft and the multifarious yet so generic and
+      potent music that deepens the kids fervor to praise and worship Him,
+      <b>THIS IS THE PLACE TO BE!</b>
+    </p>
+    <p>THINGS TO CARRY: Bible, Pen, Notebook, Bottle of water</p>
+    <h2>Goals and Aims</h2>
+    <ul className="goals">
+      <li>
+        Equip the children to be agents of perpetual change in this lost and
+        perverse generation.
+      </li>
+      <li>
+        Emphasize the importance of reading the Bible and praying in their life.
+      </li>
+      <li>
+        To impact souls and have a lasting impression in the children’s lives.
+      </li>
+    </ul>
+  </div>
+  <div className="tabs tabs-nav">
+    <ul>
+      <li><a className="activities" href="#tab-activities">Activities</a></li>
+      <li><a className="parents" href="#tab-parents">Parents</a></li>
+      <li><a className="teams" href="#tab-teams">Teams</a></li>
+      <li><a className="gallery" href="#">Gallery</a></li>
+    </ul>
+  </div>
+  <div id="activities" className="activities-tab tab-content">
+    <h1 id="tab-activities">DVBS ACTIVITES</h1>
+    <ul>
+      <li>Drills and hype sessions</li>
+      <li>Taking of Pledges</li>
+      <li>Dance and song</li>
+      <li>Praise and worship sessions</li>
+      <li>Lessons in className</li>
+      <li>Games</li>
+      <li>Snack time</li>
+      <li>Art and craft</li>
+      <li>Memory verse recitation</li>
+      <li>Puppetry</li>
+      <li>Prayer sessions</li>
+    </ul>
+    <h3>Arrival</h3>
+    <p>
+      In the morning the kids are dropped off by bus, their parents or any other
+      means fit to the venue.
+    </p>
+    <h3>Hype Session</h3>
+    <p>
+      What follows is a 15-20 min hype session outside with the kids to get them
+      warmed up. After that, they march into the sanctuary ready for drills and
+      pledges with Pst. Mtoto.
+    </p>
+    <h3>Drills and Pledges</h3>
+    <p>
+      The Bible and Christian flag. These are what we use to conduct the
+      pledges.
+    </p>
+    <ul>
+      <li><a href="#">Bible Pledge</a></li>
+      <li><a href="#">Christian Flag Pledge</a></li>
+    </ul>
+    <p>
+      This is also accompanied by the singing of a song after each respective
+      pledge.
+    </p>
+    <li>
+      <a href="#">I love the bible Stand up, stand up for Jesus </a> OR
+      <a href="#">OR I may never march in the infantry</a>
+    </li>
+
+    <h3>Praise and Worship and Dance</h3>
+    <p>
+      The children choir takes over and teaches the kids new songs and also
+      sings common ones so as to create familiarity with them.
+    </p>
+    <p>The songs are always in the same vein to the day&apos;s theme.</p>
+    <p>
+      The tremendous participation displayed by the kids in these sessions is
+      simply priceless. The goal, to teach the children and lead them to worship
+      the Lord in truth and in spirit.
+    </p>
+    <p>This is so beautiful and that is why we value these times.</p>
+    <img src="#" />
+    <h3>className Time!</h3>
+    <p>
+      After having a release prayer, the kids scurry to their respective classNamees
+      for the day&apos;s lesson based off the day&apos;s theme.
+    </p>
+    <h4>Teachers and Helpers</h4>
+    <p>
+      Our teaching staff from various churches is extremely dedicated to
+      drilling into the kids God&apos;s Word and ensuring that Christ is their firm
+      foundation and the centre of it all.
+    </p>
+    <img src="#" />
+
+    <p>
+      The teachers, helpers and the plethora of volunteers are trained by the
+      best in the industry. We as a church understand that what is ingrained
+      into the lives of these young lads and toddlers at this tender age is
+      bound to stick with them forever. Therefore, why not have only the correct
+      ideologies inculcated into every fibre of their very being by people
+      devoted to doing so. That is our creed.
+    </p>
+    <img src="#" />
+
+    <p>
+      The trustee helpers are always present in className with the teachers to help
+      alleviate any ruckus or commotion that may present itself during content
+      delivery. They too are the one responsible for facilitation of the snacks
+      per className and are present during game time.
+    </p>
+    <img src="#" />
+
+    <h3>Games</h3>
+    <p>Yay!</p>
+    <p></p>
+    <p>
+      Game time. From soccer, skipping rope, “kati”, sack racing, tag, tug of
+      war, tire plays, rubber-in-and-out jumping and many other childhood
+      classNameics, this is the time to showcase dominance and skill in what they
+      like.
+    </p>
+    <p></p>
+    <img src="#" />
+
+    <p></p>
+    <p>
+      In case of any injuries, we have a very able and alert first aid team on
+      standby which is well-equipped and, the safety of the children is their
+      primary concern.
+    </p>
+    <p></p>
+    <h3>Snack Time!!</h3>
+    <img src="#" />
+
+    <p>
+      Games are normally draining and with energy levels in kids being at an all
+      time high, they need to be juiced up again.
+    </p>
+    <p>
+      A warm cup of cocoa and a nice fluffy cake is given to every child. All
+      this, on a daily basis, ensuring that they stay psyched up and
+      reinvigorated ready for the activities to come.
+    </p>
+    <h3>Art and Craft</h3>
+    <p>
+      After a brief recap of the lesson workbook manuals are issued to the older
+      kids and to the younger ones, their colourings. There&apos;s always something
+      for everyone.
+    </p>
+    <img src="#" />
+
+    <h3>Back to the Sanctuary</h3>
+    <img src="#" />
+
+    <p>
+      The kids are taken through a brief session of song and dance by the DVBS
+      choir. A fun, action-packed jam session.
+    </p>
+    <img src="#" />
+
+    <p>
+      Now, it&apos;s time for recitations, memory work if you would. On day one each
+      child is given a portion of scripture due for recitation on the days
+      following in order to qualify as contenders for a mystery gift on Friday.
+      The earlier the recitation&apos;s done, the bigger the present.
+    </p>
+    <img src="#" />
+
+    <p>
+      Simbaaaaaa!! The kids call out as they wait for the King of the Jungle to
+      show up with his entourage.
+    </p>
+    <img src="#" />
+
+    <p>Simba, Mutuku, Kaleche, Princess, Ahmed, Twiga and Monkey.</p>
+    <img src="#" />
+
+    <p>
+      It&apos;s now time for a general recapand demo of the day&apos;s teaching by Simba
+      and crew. They always have something in store for us. Clear and concise,
+      the demo deepens further the teaching to the kids and with Twiga, Simba
+      and Monkey making their way from the “wild” just to be with the kids, it
+      all the more plausible.
+    </p>
+    <img src="#" />
+
+    <h3>Prayer Sessions</h3>
+    <p>
+      On the very last day ministers from most if not all of the churches whose
+      kids are in attendance come and give short words of enlightenment to the
+      youngins and further emphasize the importance of what&apos;s been taught to
+      them that week.
+    </p>
+    <img src="#" />
+
+    <p>
+      A few orations and it is time to commit and commission the children to go
+      and implement what they&apos;ve been entrusted with. To let their light shine
+      as it says in Matthew 5:14(You are the light of the world. A city that is
+      set on a hill cannot be hidden.)
+    </p>
+  </div>
+  <div id="parents" className="dvbs-parents-tab tab-content">
+    <h2 id="tab-parents">DVBS PARENTS</h2>
+    <p>
+      After having the entire week dedicated to the kids, after all that fun,
+      it&apos;s only fair that we give their parents a similar experience as well.
+      Or, something close to it.
+    </p>
+    <p>Therefore, over the weekend, we host the parents over for their DVBS.</p>
+    <img src="#" />
+
+    <p>
+      Same theme, same message, similar songs and even the age groups. Yep!
+      That&apos;s right, each parent chooses the age group that they wanna belong to.
+    </p>
+    <p>
+      Parents arrive mid-morning and are taken through the same regimen as their
+      kids that week. From the drills, to the hype and even puppetry.
+    </p>
+    <img src="#" />
+
+    <p>
+      Theirs however is a more compact and sophisticated mega lesson delivered
+      to them by a number of various speakers.
+    </p>
+    <img src="#" />
+
+    <p>
+      They are apprised on how to further infuse and build upon what their kids
+      have been taught as they are inevitably the ones who spend most time with
+      them.
+    </p>
+    <h4>Parents, you cannot afford to miss this!</h4>
+    <p>
+      Proverbs 22:6 ~ Train up a child in the ways he should go; even when he is
+      old, he will not depart from it.(ESV)
+    </p>
+  </div>
+  <div id="teams" className="dvbs-teams-tab tab-content">
+    <h2 id="tab-teams">DVBS TEAMS</h2>
+    <p>
+      The massive success and result obtained could never be possible without
+      the participation of ALL the church&apos;s ministries and partners as well.
+    </p>
+    <p>However, big big shoutout to these ministries especially:</p>
+    <section className="ushering-hospitality">
+      <h3>Ushering and hospitality</h3>
+      <p>These are simply put, the most devout amongst the whole lot.</p>
+      <p>
+        From their early arrivals, clocking in as early as 6:00AM to their
+        outlandishly surreal coordination in preparation of the meals, drinks
+        and assortment of the goodies, these guys work cannot go unnoticed
+      </p>
+      <img src="#" />
+
+      <p>
+        Their dedication and levels of impeccability are unparralled.
+        <b>BIG UPS!</b>
+      </p>
+      <img src="#" />
+    </section>
+    <section className="transport-security">
+      <h3>Transport and Security</h3>
+      <p>
+        Is my child safe? How will my child get back home? How will they get
+        there?
+      </p>
+      <p>
+        <strong>DO NOT FRET!</strong>These are the only questions lingering in
+        the minds of these men. Responsible for the transportation of your child
+        to and fro the venue, safety is their number one priority. (Pics)
+      </p>
+      <p>
+        Also, while here at the venue, heh! No one with any malevolent or ill
+        intentions is getting past these guys. After all your child&apos;s safety is
+        the only reason they&apos;re here to begin with. (Pics)
+      </p>
+      <p>
+        “Rest assured. Your child is in safe hands” ~ ….(Head of security)(Pics)
+      </p>
+    </section>
+    <section className="health-sanitation">
+      <h3>Health and sanitation</h3>
+      <p>
+        From tiny headaches to any sort of bruises, they are here for your
+        child.
+      </p>
+      <p>
+        Clean sanctuary, even cleaner restrooms and water for handwashing, they
+        got it all covered. We take the well-being of all the children upon our
+        shoulders and this is why our team of qualified doctors, first raiders
+        and clean up crew are so fixated on the same. “...” … (Head of team and
+        certified Dr.)
+      </p>
+      <img src="#" />
+    </section>
+  </div>
+  </div>
+
+)
 }
