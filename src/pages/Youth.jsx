4311defc928031384d518e43affefd827c7a9a49
Ronald Mutua <ronaldmutua617@gmail.com>
2024-08-27 23:35:01 +0300

    created the youth ministry jsx

---
+++ src/pages/Youth.jsx
@@ -0,0 +1,48 @@
+
+export default function Youth(){
+
+    return (
+        <div className="all">
+    <div className="container">
+        <h1>Youth Ministry</h1>
+        <div className="leader">
+            <img src="#" />
+            <div className="leader-name">
+                <h3>Allan Alliato</h3>
+                <h4>Ministry Leader</h4>
+            </div>    
+        </div>
+        <p>Welcome all to the TAMI Youth page. As the youth we have the following activities:
+            </p>
+        <ul>
+            <li>Cookouts</li>
+            <li>Sleepovers and game nights</li>
+            <li>Bonfires</li>
+            <li>Prayer walks</li>
+            <li>Rise Up</li>
+            <li>Plays</li>
+            <li>Games</li>
+            <li>Debates and discussions</li>
+            <li>Concerts, worship experiences and street worship</li>
+        </ul>
+        <p>Every third Sunday each month, the service is youth-based as we take the church through a time of Praise and worship. Everything from intercessory sessions in the morning to the serving of tea after the service, this is done entirely by us.</p>
+        <p>After the service later in the afternoon, we have the “Rise Up”. An entire youth service that&apos;s open to all the youth in Machakos.</p>
+        <div className="photos">
+            <img src="#" />
+            <img src="#" />
+        </div>
+        <p>Here we have sessions of praise and worship, debates and discussions on sensitive topics and whatever&apos;s currently affecting us as the youth and society as a whole.</p>
+        <p>As the Rise Up, we are also keen on talent promotion. Dance, spoken word,song and anything we as the youth can do to further the message of Christ. This makes it a suitable platform for anyone to display the talent they have each month. <b>ALL ARE WELCOME.</b></p>
+        <p>Apart from Sundays, we also plan for other fun and engaging activities like hikes, prayer walks, dinners, sleepovers, cookouts, sports days and much more. We have a very capable team that&apos;s responsible for the planning and facilitation of all the activities we suggest and plan for.</p>
+        <div className="photos">
+            <img src="#" />
+            <img src="#" />
+        </div>
+        <p>To all who would want to be a part of this movement and fast-growing society of young spiritually endowed Christians, we welcome you all with arms open wide. Tag sling with all your friends.</p>
+        <p>Snacks and refreshments are provided.</p>
+        <p>Remember! <b>KEEP RISING!!</b></p>
+    </div>
+    </div>
+
+    )
+}
