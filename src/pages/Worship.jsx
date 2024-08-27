e392c9af63cd3d38a80215c63308add85a2018ee
Ronald Mutua <ronaldmutua617@gmail.com>
2024-08-27 23:28:06 +0300

    created the worship jsx

---
+++ src/pages/Worship.jsx
@@ -0,0 +1,32 @@
+
+
+export default function Worship(){
+    return (
+        <div className="all">
+    <div className="container">
+        <h1>Worship Ministry</h1>
+        <div className="leader">
+            <img src="#" />
+            <div className="leader-name">
+                <h3>Geoffrey Oeri</h3>
+                <h4>Ministry Head</h4>
+            </div>    
+        </div>
+        <p>Worship ministry is basically a calling. Our vision is to  serve through music to the glory of God. Our most active goal currently is frequently holding worship experiences.
+            Further enabling people to deepen their relationship with the Lord through our music.
+           </p>
+        <p>Our activities are mostly driven to edify us.</p>
+        <h5>Our teams and activities;</h5>
+        <ul>
+            <li>Media team</li>
+            <li>Prayer walks</li>
+            <li>Weekly online prayer meetings</li>
+            <li>Worship experiences</li>
+            <li>Bonding day outs and luncheons</li>
+            <li>Get together sleepovers</li>
+        </ul>
+    </div>
+    </div>
+
+    )
+}
